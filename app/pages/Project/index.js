153f1c70a8e36bd11097a9a0a6d243509d5cb049
Luka Rakic <Luka>
2023-04-01 12:17:21 +0200

    fixed resposive for project page

--- app/pages/Project/index.js
+++ app/pages/Project/index.js
@@ -1,5 +1,7 @@
 import Page from 'classes/Page';
 
+import each from 'lodash/each';
+
 export default class Project extends Page {
   constructor() {
     super({
@@ -8,7 +10,22 @@ export default class Project extends Page {
       element: '.project',
       elements: {
         wrapper: '.project__wrapper',
+        accordionButtons: '.project__accordion__button',
+        accordion: '.project__accordion',
       },
     });
+
+    this.create();
+    this.accordionListener();
+  }
+
+  onClick(e) {
+    if (!e.target.classList.contains('project__accordion__header')) {
+      return;
+    }
+  }
+
+  accordionListener() {
+    this.elements.accordion.addEventListener('click', this.onClick);
   }
 }
