c70c4631db92da330af3b44568ba78f8f27a9430
Luka Rakic <Luka>
2023-04-01 12:21:12 +0200

    fixed resposive for project page

--- app/pages/Project/index.js
+++ app/pages/Project/index.js
@@ -10,22 +10,7 @@ export default class Project extends Page {
       element: '.project',
       elements: {
         wrapper: '.project__wrapper',
-        accordionButtons: '.project__accordion__button',
-        accordion: '.project__accordion',
       },
     });
-
-    this.create();
-    this.accordionListener();
-  }
-
-  onClick(e) {
-    if (!e.target.classList.contains('project__accordion__header')) {
-      return;
-    }
-  }
-
-  accordionListener() {
-    this.elements.accordion.addEventListener('click', this.onClick);
   }
 }
